696201004072781197e27d44b3de7e58c93fa83c
Winter Thane <saso.basic.2012@gmail.com>
2022-01-23 17:34:30 +0100

    working on basiskurs

---
+++ UltraschallKursAnmeldung/Models/BasisKursModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UltraschallKursAnmeldung.Models
+{
+    public class BasisKursModel
+    {
+        [Required]
+        public bool IsInWeiterbildung { get; set; }
+        [Required]
+        public string Anrede { get; set; }
+        [Required]
+        public string Fachtitel { get; set; }
+        [Required]
+        public string Vorname { get; set; }
+        [Required]
+        public string Nachname { get; set; }
+        [Required]
+        public string Strasse { get; set; }
+        [Required]
+        public string StrasseNr { get; set; }
+        [Required]
+        public int ZipCode { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string PhonePrivate { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailPrivate { get; set; }
+        public string PhoneWork { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailWork { get; set; }
+
+    }
+}
